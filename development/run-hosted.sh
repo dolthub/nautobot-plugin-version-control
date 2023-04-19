87517982d8c2c308693676e9ef9d8efc5d03b690
Pavel Safronov <pavel@dolthub.com>
2023-04-19 14:22:39 -0700

    Introduced a separate script for running hosted.

--- development/run-hosted.sh
+++ development/run-hosted.sh
@@ -33,14 +33,34 @@ run_command() {
 }
 
 # Clean up
+echo "Cleaning up..."
 rm -rf logs
 mkdir logs
 
 # Pre-flight checks
+echo "Checking for required files..."
 if [ ! -f "./development/creds.env" ]; then
   echo "./development/creds.env file not found. Please create it - for example, by copying ./development/creds.example.env - and try again."
   exit 1
 fi
+if [ ! -f "./development/hosted_ca.pem" ]; then
+  echo "./development/hosted_ca.pem file not found. Please download a copy from https://hosted.doltdb.com/deployments/ and try again."
+  exit 1
+fi
+
+# Load environment variables
+echo "Loading environment variables..."
+source ./development/creds.env
+
+# Create databases
+echo "Creating databases..."
+mysql -h"$DOLT_HOST" -u"$DOLT_USER" -p"$DOLT_PASSWORD" -e "DROP DATABASE IF EXISTS nautobot;"
+mysql -h"$DOLT_HOST" -u"$DOLT_USER" -p"$DOLT_PASSWORD" -e "CREATE DATABASE nautobot CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;"
+mysql -h"$DOLT_HOST" -u"$DOLT_USER" -p"$DOLT_PASSWORD" -e "DROP DATABASE IF EXISTS test_nautobot;"
+mysql -h"$DOLT_HOST" -u"$DOLT_USER" -p"$DOLT_PASSWORD" -e "CREATE DATABASE test_nautobot CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;"
+mysql -h"$DOLT_HOST" -u"$DOLT_USER" -p"$DOLT_PASSWORD" -e "DROP DATABASE IF EXISTS test_nautobot_hosted;"
+mysql -h"$DOLT_HOST" -u"$DOLT_USER" -p"$DOLT_PASSWORD" -e "CREATE DATABASE test_nautobot_hosted CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;"
+echo "Done creating databases!"
 
 # Run commands
 run_command "invoke destroy"
@@ -49,5 +69,5 @@ run_command "invoke migrate"
 run_command "invoke load-data"
 run_command "invoke start"
 
-Notify user
-osascript -e "tell app \"System Events\" to display dialog \"Done!\""
+# Notify user
+# osascript -e "tell app \"System Events\" to display dialog \"Done!\""
